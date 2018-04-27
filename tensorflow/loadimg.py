ed8c17df50063107afdc2954843ee42a4808929f
Your Name <you@example.com>
2018-04-27 10:22:13 +0900

    update

---
+++ tensorflow/loadimg.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+
+import os
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing.image import load_img, list_pictures, img_to_array
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+
+COMMONDIR = '../common/make_image'
+TRAINDIR = os.path.join(COMMONDIR, 'train')
+TESTDIR = os.path.join(COMMONDIR, 'test')
+
+def loadimg_one(DIRPATH):
+    x = []
+    y = []
+    img_list = os.listdir(DIRPATH)
+    img_count = 0
+
+    for number in img_list:
+        dirpath = os.path.join(DIRPATH, number)
+        for picture in list_pictures(dirpath):
+            img = img_to_array(load_img(picture, target_size=(64, 64)))
+            x.append(img)
+            y.append(img_count)
+            #print("Load {0} : {1}".format(picture, img_count))
+        img_count += 1
+
+    output_count = img_count
+    x = np.asarray(x)
+    x = x.astype('float32')
+    x = x/255.0
+    y = np.asarray(y)
+    #y = np_utils.to_categorical(y, output_count)
+
+    return x, y, output_count
+
+
+def loadimg():
+    x_train, y_train, class_count = loadimg_one(TRAINDIR)
+    x_test,  y_test,  _  = loadimg_one(TESTDIR)
+    print(x_test.shape)
+    #for i in range(0, x_test.shape[0]):
+    #    plt.imshow(x_test[i])
+    #    plt.show()
+
+    return x_train,  y_train, x_test, y_test, class_count
+
+if __name__ == '__main__':
+    loadimg()
